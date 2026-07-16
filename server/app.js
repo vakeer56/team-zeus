ce4c3f18be91b74fcd3d8305fb9532d1ec852121
vakeer56 <mailvarun17@gmail.com>
2026-07-16 11:02:33 +0530

    chore: add db connections

--- server/app.js
+++ server/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 require('dotenv').config();
 
 const PORT=process.env.PORT || 3000;
+
+
+
+//-----------------------------REMOVE THE CONSOLE MSSGE LATER -------------------------------
+
+mongoose.connect(process.env.DB_URL)
+    .then( ()=> console.log("connected to the db...."))
+    .catch( err => console.log(err)) ;
 
 app.get('/', (req, res) => {
     res.send('app is alive')
